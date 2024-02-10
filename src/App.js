2403096e1d8ca3a9de29d9fe83e06f7675ccca74
Ben Ng <cngaa@amazon.com>
2024-02-10 10:54:41 -0500

    added Navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Page1 from './Page1'; // Your first page component
-import Page2 from './Page2'; // Your second page component
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Navigation from './Navigation'; // Your Navigation component
+import Page1 from './Page1';
+import Page2 from './Page2';
 // Import more pages as needed
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Page1} />
-        <Route path="/page2" component={Page2} />
-        {/* Define more routes as needed */}
-      </Switch>
+      <div>
+        <Navigation /> {/* Global navigation */}
+        <Route render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={300} classNames="slide">
+              <Switch location={location}>
+                <Route exact path="/" component={Page1} />
+                <Route path="/page2" component={Page2} />
+                {/* Define more routes as needed */}
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )} />
+      </div>
     </Router>
   );
 }
