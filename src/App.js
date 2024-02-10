b434bac51558086b330b9b5021d22841859cf447
Ben Ng <cngaa@amazon.com>
2024-02-10 10:54:09 -0500

    added React Routers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Page1 from './Page1'; // Your first page component
+import Page2 from './Page2'; // Your second page component
+// Import more pages as needed
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Page1} />
+        <Route path="/page2" component={Page2} />
+        {/* Define more routes as needed */}
+      </Switch>
+    </Router>
   );
 }
 
